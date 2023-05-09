c91a48ee1e7d85f06544142ba33be7dc01457626
erickjuan50 <erickjuan50@gmail.com>
2023-05-09 16:01:23 -0300

    c03 09.05

--- UrnaEletronica/src/GUI/MainScreen.java
+++ UrnaEletronica/src/GUI/MainScreen.java
@@ -12,7 +12,7 @@ public class MainScreen{
         mainScreen.setResizable(false);
         mainScreen.setLocationRelativeTo(null);
 
-        ImageIcon background = new ImageIcon("Urna Eletronica\src\Arquivos\Main_Screen.png");
+        ImageIcon background = new ImageIcon("\UrnaEletronica\src\Arquivos\Main_Screen.png");
         JLabel label1 = new JLabel(background);
         label1.setBounds(0, 0, background.getIconWidth(), background.getIconHeight());
         mainScreen.getContentPane().add(label1);
--- UrnaEletronica/src/Vilao.java
+++ UrnaEletronica/src/Vilao.java
@@ -1,4 +1,5 @@
 public class Vilao{
+    private String nome;
     private String codVilao;
     private int votos;
     private String Hash;
