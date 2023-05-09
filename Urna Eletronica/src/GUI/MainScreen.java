d65feff7c76d3fd402dfb029e0f989c242aef3ac
JPacademico <ximbas@academico.ufs.br>
2023-05-09 15:57:26 -0300

    c02
    add uma base da IG

---
+++ Urna Eletronica/src/GUI/MainScreen.java
@@ -0,0 +1,23 @@
+package GUI;
+
+import javax.swing.*;
+
+
+public class MainScreen{
+    public MainScreen(){
+        JFrame mainScreen = new JFrame("Login");
+        mainScreen.setVisible(true);
+        mainScreen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        mainScreen.setResizable(false);
+        mainScreen.setLocationRelativeTo(null);
+
+        ImageIcon background = new ImageIcon("Urna Eletronica\src\Arquivos\Main_Screen.png");
+        JLabel label1 = new JLabel(background);
+        label1.setBounds(0, 0, background.getIconWidth(), background.getIconHeight());
+        mainScreen.getContentPane().add(label1);
+        mainScreen.setSize(background.getIconWidth(), background.getIconHeight());
+    }
+
+    
+}
---
+++ Urna Eletronica/src/GUI/Teste.java
@@ -0,0 +1,8 @@
+package GUI;
+
+public class Teste {
+    public static void main(String[] args) {
+        MainScreen teste = new MainScreen();
+    }
+    
+}
