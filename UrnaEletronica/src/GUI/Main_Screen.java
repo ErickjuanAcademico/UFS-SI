f70f2eb069dca63aa02b4495e335e1d17ce57345
erickjuan50 <erickjuan50@gmail.com>
2023-05-10 01:02:13 -0300

    Revert "c07"
    This reverts commit 4eaa8274a863da40eff76598c9855b7402166194.

--- UrnaEletronica/src/GUI/Main_Screen.java
+++ UrnaEletronica/src/GUI/Main_Screen.java
@@ -1,18 +1,13 @@
 package GUI;
 
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import javax.swing.*;
 import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
 
-public class Main_Screen implements ActionListener{
-
-    JTextField cadastro;
-
+public class Main_Screen {
     public Main_Screen(){
         BufferedImage background = null;
 
@@ -42,27 +37,11 @@ public class Main_Screen implements ActionListener{
         JButton confirmar = new JButton("Confirmar");
         confirmar.setBounds(535, 542, 207, 60);
         label.add(confirmar);
-        confirmar.addActionListener(this);
-        
-        cadastro = new JTextField();
-        cadastro.setBounds(430, 442, 420, 45);
-
-        label.add(cadastro);
-
-
 
         
-    }
-    private String frase;
-    @Override
-    public void actionPerformed(ActionEvent e){
-        if(cadastro.getText().length()==11){
-            frase = "Confirmado.";
-            JOptionPane.showMessageDialog(null, frase, "Verificaçao", JOptionPane.INFORMATION_MESSAGE, null);
-        } else{
-            frase = "Cadastro invalido.";
-            JOptionPane.showMessageDialog(null, frase, "Verificaçao", JOptionPane.ERROR_MESSAGE, null);
-        }
+
+
+
         
     }
     
