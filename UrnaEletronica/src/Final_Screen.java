c18f6568227abf2d54866b61b8cb576c3abba7c3
JPacademico <ximbas@academico.ufs.br>
2023-05-13 03:43:03 -0300

    atualização

--- UrnaEletronica/src/Final_Screen.java
+++ UrnaEletronica/src/Final_Screen.java
@@ -12,24 +12,120 @@ import javax.imageio.ImageIO;
 
 public class Final_Screen{
 
-    JLabel venceu;
+    JLabel venceu1;
+    JLabel venceu2;
+    JLabel venceu3;
+    JLabel venceu4;
+    JLabel venceu5;
+    JLabel venceu6;
     
     public Final_Screen(){
+        BufferedImage imagem1 = null;
+        BufferedImage imagem2 = null;
+        BufferedImage imagem3 = null;
+        BufferedImage imagem4 = null;
+        BufferedImage imagem5 = null;
+        BufferedImage imagem6 = null;
+
+        try {
+            imagem1 = ImageIO.read(new File("UrnaEletronica\\src\\GUI\\Arquivos\\Darth_vader.jpg"));
+        }
+        catch (IOException erro) {
+            erro.printStackTrace();
+        }
+        try {
+            imagem2 = ImageIO.read(new File("UrnaEletronica\\src\\GUI\\Arquivos\\Coringa.jpg"));
+        }
+        catch (IOException erro) {
+            erro.printStackTrace();
+        }
+        try {
+            imagem3 = ImageIO.read(new File("UrnaEletronica\\src\\GUI\\Arquivos\\Dr_octopus.jpg"));
+        }
+        catch (IOException erro) {
+            erro.printStackTrace();
+        }
+        try {
+            imagem4 = ImageIO.read(new File("UrnaEletronica\\src\\GUI\\Arquivos\\Voldemort.jpg"));
+        }
+        catch (IOException erro) {
+            erro.printStackTrace();
+        }
+        try {
+            imagem5 = ImageIO.read(new File("UrnaEletronica\\src\\GUI\\Arquivos\\Thano.jpg"));
+        }
+        catch (IOException erro) {
+            erro.printStackTrace();
+        }
+        try {
+            imagem6 = ImageIO.read(new File("UrnaEletronica\\src\\GUI\\Arquivos\\Satanas.jpg"));
+        }
+        catch (IOException erro) {
+            erro.printStackTrace();
+        }
 
         JFrame telaFinal = new JFrame("Quem venceu?");
-        telaFinal.setVisible(true);
-        telaFinal.setSize(800, 500);
+        
+        telaFinal.setSize(1280, 720);
         telaFinal.setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         telaFinal.setLocationRelativeTo(null);
         telaFinal.setResizable(false);
         telaFinal.setLayout(null);
 
-        venceu = new JLabel("placeholder!!");
-        venceu.setBounds(200, 200, 500, 100);
-        venceu.setFont(new Font("Arial", Font.BOLD, 70));
-        venceu.setVisible(true);
-        telaFinal.add(venceu);
+        Image vader = imagem1.getScaledInstance(telaFinal.getWidth(), telaFinal.getHeight(), 1);
+        Image joker = imagem2.getScaledInstance(telaFinal.getWidth(), telaFinal.getHeight(), 1);
+        Image drOctopus = imagem3.getScaledInstance(telaFinal.getWidth(), telaFinal.getHeight(), 1);
+        Image voldemort = imagem4.getScaledInstance(telaFinal.getWidth(), telaFinal.getHeight(), 1);
+        Image thano = imagem5.getScaledInstance(telaFinal.getWidth(), telaFinal.getHeight(), 1);
+        Image satan = imagem6.getScaledInstance(telaFinal.getWidth(), telaFinal.getHeight(), 1);
 
+        venceu1 = new JLabel();
+        venceu2 = new JLabel();
+        venceu3 = new JLabel();
+        venceu4 = new JLabel();
+        venceu5 = new JLabel();
+        venceu6 = new JLabel();
+
+        ImageIcon vencedor1 = new ImageIcon(vader);
+        venceu1.setBounds(telaFinal.getX()/2 - 20, telaFinal.getY()/2, 1280, 720);
+        venceu1.setIcon(vencedor1);
+        venceu1.setVisible(false);
+
+        ImageIcon vencedor2 = new ImageIcon(joker);
+        venceu2.setBounds(telaFinal.getX()/2 - 20, telaFinal.getY()/2, 1280, 720);
+        venceu2.setIcon(vencedor2);
+        venceu2.setVisible(false);
+
+        ImageIcon vencedor3 = new ImageIcon(drOctopus);
+        venceu3.setBounds(telaFinal.getX()/2 - 20, telaFinal.getY()/2, 1280, 720);
+        venceu3.setIcon(vencedor3);
+        venceu3.setVisible(false);
+
+        ImageIcon vencedor4 = new ImageIcon(voldemort);
+        venceu4.setBounds(telaFinal.getX()/2 - 20, telaFinal.getY()/2, 1280, 720);
+        venceu4.setIcon(vencedor4);
+        venceu4.setVisible(false);
+
+        ImageIcon vencedor5 = new ImageIcon(thano);
+        venceu5.setBounds(telaFinal.getX()/2 - 20, telaFinal.getY()/2, 1280, 720);
+        venceu5.setIcon(vencedor5);
+        venceu5.setVisible(true);
+
+        ImageIcon vencedor6 = new ImageIcon(satan);
+        venceu6.setBounds(telaFinal.getX()/2 - 20, telaFinal.getY()/2, 1280, 720);
+        venceu6.setIcon(vencedor6);
+        venceu6.setVisible(false);
+
+        
+        telaFinal.setVisible(true);
+        telaFinal.add(venceu1);
+        telaFinal.add(venceu2);
+        telaFinal.add(venceu3);
+        telaFinal.add(venceu4);
+        telaFinal.add(venceu5);
+        telaFinal.add(venceu6);
+        
+        
 
     }
     
